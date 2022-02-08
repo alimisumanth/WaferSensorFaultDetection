3930ae82103ce13df694a4803e69dd22a1174c11
alimi <swarthik729@gmail.com>
2022-02-09 00:46:19 +0530

    File update

--- DataIngestion/dataIngestion.py
+++ DataIngestion/dataIngestion.py
@@ -1,3 +1,26 @@
+# -*- coding: utf-8 -*-
+
+"""
+=============================================================================
+Created on: 08-02-2022 11:23 PM
+Created by: ASK
+=============================================================================
+
+Project Name: WaferSensorFaultDetection
+
+File Name: dataIngestion.py
+
+Description : This module contains various kinds of ingestion operations
+
+Version: 1.0
+
+Revision: None
+
+=============================================================================
+"""
+
+# importing required libraries
+
 from DataPreProcessing import PreProcessing
 from Utils import Utils
 from InputValidation import InputValidation
@@ -7,13 +30,34 @@ import shutil
 
 
 class DataIngestion:
+    """
+    Transfers the data from one source to destination.
+
+    Methods :
+
+    rawDataLocal(path):
+        Transfers data from local source to Data/rawData
+    LoadToDB():
+        Transfers data to Database
+    LoadFromDB():
+        Transfers data from database
+
+    """
     def __init__(self):
+
         self.preprocessing = PreProcessing.PreProcessing()
         self.utils = Utils.utils()
         self.inputValidation = InputValidation.inputValidation()
         self.dbOperations = databaseOperations.Database()
 
     def rawDataLocal(self, path):
+        """
+        Transfers data for the path provided in argument to Data/rawData folder
+
+        :param path: Local folder path where files are available
+
+        :return: None
+        """
         rawdata = 'Data/rawData'
         self.utils.dircheck(rawdata)
         files = [i for i in os.listdir(path) if i.endswith('.csv')]
@@ -22,6 +66,11 @@ class DataIngestion:
             shutil.copy(srcpath, rawdata)
 
     def LoadToDB(self):
+        """
+        Transfers data into database
+
+        :return: None
+        """
         self.inputValidation.Filevalidation()
         self.inputValidation.columnValidation()
         session = self.dbOperations.DBConnection()
@@ -29,6 +78,11 @@ class DataIngestion:
         session.close()
 
     def LoadFromDB(self):
+        """
+        Loads data from database
+
+        :return: dataframe
+        """
         session = self.dbOperations.DBConnection()
         dataFrame = self.dbOperations.LoadFromDB(session)
         session.close()
--- databaseOperations/databaseOperations.py
+++ databaseOperations/databaseOperations.py
@@ -1,3 +1,25 @@
+# -*- coding: utf-8 -*-
+
+"""
+=============================================================================
+Created on: 08-02-2022 11:23 PM
+Created by: ASK
+=============================================================================
+
+Project Name: WaferSensorFaultDetection
+
+File Name: databaseOperations.py
+
+Description : databaseOperations contains various operations performed on database.
+
+Version: 1.0
+
+Revision: None
+
+=============================================================================
+"""
+
+# importing required libraries
 from Utils import Utils
 import sqlite3
 import os
@@ -5,14 +27,47 @@ import pandas as pd
 
 
 class Database:
+    """
+        Description: Database class contains various operations performed on sqlite database
+
+        Attributes: None
+
+        Methods:
+
+        DBConnection():
+            Creates a new connection to the database
+        LoadtoDB(session):
+            Loads data into database
+        LoadFromDB(session):
+            Loads data from database
+        dropTable(session):
+            drops table from database
+
+    """
     def __init__(self):
+        """
+            Constructor initializes Utils object
+        """
+
         self.utils = Utils.utils()
 
     def DBConnection(self):
+        """
+        Creates a new sqlite database connection to db.sqlite3 database
+        :return: sqlite object
+        """
         session = sqlite3.connect('db.sqlite3')
         return session
 
     def LoadtoDB(self, session):
+        """
+        Reads files in Data/goodData directory and generates a dataframe.
+        Generated dataframe is stored in database which can be used later for further processing of data
+
+        :param session: sqlite database connection object
+
+        :return: None
+        """
         goodData = 'Data/goodData'
         self.utils.dircheck(goodData)
         files = [i for i in os.listdir(goodData)]
@@ -23,9 +78,23 @@ class Database:
         dataframe.to_sql('wafer', session, if_exists='replace')
 
     def LoadFromDB(self, session):
+        """
+        Loads data from the database created using LoadtoDB method for further processing of data
+
+        :param session: sqlite database connection object
+
+        :return: wafer data from database(dataframe)
+        """
         df = pd.read_sql_query("select * from wafer", session)
         return df
 
     def dropTable(self, session):
+        """
+        Drops an existing table from the database
+
+        :param session: sqlite database connection object
+
+        :return: None
+        """
         query = 'DROP TABLE IF EXISTS  wafer'
         session.execute(query)
